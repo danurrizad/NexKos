8f676cfdcd172eafe0447cfe08948ca294e7e634
Danurrizad Ryan Darmoko <79238162+danurrizad@users.noreply.github.com>
2025-05-06 10:05:10 +0700

    delete: config file

--- frontend/src/utils/config.ts
+++
@@ -1,7 +0,0 @@
-const config = {    
-    // Server Bagus
-    // BACKEND_URL: 'http://10.65.132.69:5005',
-    BACKEND_URL: 'https://85xlx9xl-5005.asse.devtunnels.ms/',
-  }
-  
-export default config
