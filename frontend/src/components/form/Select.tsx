054fa75d92c91c255d236a5222a65d38a7ec5565
danurrizad <danurrizad242@gmail.com>
2025-05-14 10:38:57 +0700

    feat: add reusable components

--- frontend/src/components/form/Select.tsx
+++ frontend/src/components/form/Select.tsx
@@ -1,3 +1,4 @@
+import { ChevronDownIcon } from "@/icons";
 import React, { useState } from "react";
 
 interface Option {
@@ -11,6 +12,7 @@ interface SelectProps {
   onChange: (value: string) => void;
   className?: string;
   defaultValue?: string;
+  showPlaceholder?: boolean
 }
 
 const Select: React.FC<SelectProps> = ({
@@ -19,6 +21,7 @@ const Select: React.FC<SelectProps> = ({
   onChange,
   className = "",
   defaultValue = "",
+  showPlaceholder = true
 }) => {
   // Manage the selected value
   const [selectedValue, setSelectedValue] = useState<string>(defaultValue);
@@ -30,34 +33,41 @@ const Select: React.FC<SelectProps> = ({
   };
 
   return (
-    <select
-      className={`h-11 w-full appearance-none rounded-lg border border-gray-300  px-4 py-2.5 pr-11 text-sm shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 ${
-        selectedValue
-          ? "text-gray-800 dark:text-white/90"
-          : "text-gray-400 dark:text-gray-400"
-      } ${className}`}
-      value={selectedValue}
-      onChange={handleChange}
-    >
-      {/* Placeholder option */}
-      <option
-        value=""
-        disabled
-        className="text-gray-700 dark:bg-gray-900 dark:text-gray-400"
+    <div className="relative">
+      <select
+        className={`h-11 w-full cursor-pointer appearance-none rounded-lg border   px-4 py-2.5  text-sm shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 ${
+          selectedValue
+            ? "text-gray-800 dark:text-white/90"
+            : "text-gray-400 dark:text-gray-400"
+        } ${className}`}
+        value={selectedValue}
+        onChange={handleChange}
       >
-        {placeholder}
-      </option>
-      {/* Map over options */}
-      {options.map((option) => (
-        <option
-          key={option.value}
-          value={option.value}
-          className="text-gray-700 dark:bg-gray-900 dark:text-gray-400"
-        >
-          {option.label}
-        </option>
-      ))}
-    </select>
+        {/* Placeholder option */}
+        { showPlaceholder && (
+          <option
+            value=""
+            disabled
+            className="text-gray-700 dark:bg-gray-900 dark:text-gray-400 cursor-pointer"
+          >
+            {placeholder}
+          </option>
+        )}
+        {/* Map over options */}
+        {options.map((option) => (
+          <option
+            key={option.value}
+            value={option.value}
+            className="text-gray-700 dark:bg-gray-900 dark:text-gray-400 cursor-pointer"
+          >
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <div className="absolute right-2 top-3.25">
+        <ChevronDownIcon/> 
+      </div>
+    </div>
   );
 };
 
---
+++ frontend/src/components/ui/icon/IconDisplay.tsx
@@ -0,0 +1,64 @@
+import {
+    BadgeIcon, CctvIcon, ChairIcon, CheckroomIcon, ClimateIcon,
+    CookingIcon, CurtainIcon, DeskIcon, DishwasherIcon, DoorSlideIcon,
+    DoubleBedIcon, ElectricityIcon, FanIcon, GuardianIcon, KeyIcon,
+    KitchenIcon, LaundryIcon, ManIcon, MosqueIcon, ParkingIcon,
+    PrayerIcon, ShowerIcon, SingleBedIcon, SmokeIcon, SmokingRoomIcon,
+    TvIcon, WaterIcon, WaterHeaterIcon, WcIcon, WifiIcon, WomanIcon,
+  } from "@/icons";
+import { JSX } from "react";
+
+  type IconItem = {
+    render: JSX.Element;
+    value: string;
+  };
+
+  interface IconDisplayProps{
+    iconName: string
+  }
+  
+const IconDisplay: React.FC<IconDisplayProps> = ({
+    iconName
+}) => {
+    const iconList: IconItem[] = [
+        { render: <BadgeIcon />, value: "badge" },
+        { render: <CctvIcon />, value: "cctv" },
+        { render: <ChairIcon />, value: "chair" },
+        { render: <CheckroomIcon />, value: "checkroom" },
+        { render: <ClimateIcon />, value: "climate" },
+        { render: <CookingIcon />, value: "cooking" },
+        { render: <CurtainIcon />, value: "curtain" },
+        { render: <DeskIcon />, value: "desk" },
+        { render: <DishwasherIcon />, value: "dishwasher" },
+        { render: <DoorSlideIcon />, value: "door-sliding" },
+        { render: <DoubleBedIcon />, value: "double-bed" },
+        { render: <ElectricityIcon />, value: "electricity" },
+        { render: <FanIcon />, value: "fan" },
+        { render: <GuardianIcon />, value: "guardian" },
+        { render: <KeyIcon />, value: "key" },
+        { render: <KitchenIcon />, value: "kitchen" },
+        { render: <LaundryIcon />, value: "laundry" },
+        { render: <ManIcon />, value: "man" },
+        { render: <MosqueIcon />, value: "mosque" },
+        { render: <ParkingIcon />, value: "parking" },
+        { render: <PrayerIcon />, value: "prayer" },
+        { render: <ShowerIcon />, value: "shower" },
+        { render: <SingleBedIcon />, value: "single-bed" },
+        { render: <SmokeIcon />, value: "smoke" },
+        { render: <SmokingRoomIcon />, value: "smoking-room" },
+        { render: <TvIcon />, value: "tv" },
+        { render: <WaterIcon />, value: "water" },
+        { render: <WaterHeaterIcon />, value: "water-heater" },
+        { render: <WcIcon />, value: "wc" },
+        { render: <WifiIcon />, value: "wifi" },
+        { render: <WomanIcon />, value: "woman" },
+      ];
+
+    return(
+        <>
+            { iconList.find((data)=>data.value === iconName)?.render }
+        </>
+    )
+}
+
+export default IconDisplay
