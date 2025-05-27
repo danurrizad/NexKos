48ec5a3e350a2376a1c25f42a40a834a8d35d8eb
danurrizad <danurrizad242@gmail.com>
2025-05-27 17:52:19 +0700

    config: add domain for Image backend url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["85xlx9xl-5005.asse.devtunnels.ms"], // <-- Add your hostname here
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
